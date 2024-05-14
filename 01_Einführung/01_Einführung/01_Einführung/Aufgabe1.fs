dac476e049bb15fe057962184637ca12a4712772
Kay Schnyder <schnyderkay@gmail.com>
2024-05-14 10:18:31 +0200

    fix

--- 01_Einführung/01_Einführung/01_Einführung/Aufgabe1.fs
+++ 01_Einführung/01_Einführung/01_Einführung/Aufgabe1.fs
@@ -1,2 +1,16 @@
-﻿let getDiscountPercentage (shoppingList: list<string>) = 
-    List.exists (fun i -> i = "book") shoppingList
+﻿let calculateScore (word: string) =
+    word.Replace("a", "")
+         .ToCharArray()
+         |> Array.length
+         |> (+) 0
+
+let wordCount (word:string) = 
+    String.length word
+
+let print (word: string, count: int) = 
+    printfn "'%s' has %d letters" word count
+
+print ("imperative", calculateScore "imperative")
+print ("no", calculateScore "no")
+print ("declarative", wordCount "declarative")
+print ("yes", wordCount "yes")
--- 01_Einführung/01_Einführung/01_Einführung/Aufgabe2.fs
+++ 01_Einführung/01_Einführung/01_Einführung/Aufgabe2.fs
@@ -1,2 +1,2 @@
-﻿module Aufgabe2
-
+﻿let getDiscountPercentage (shoppingList: list<string>) = 
+    List.exists (fun i -> i = "book") shoppingList
