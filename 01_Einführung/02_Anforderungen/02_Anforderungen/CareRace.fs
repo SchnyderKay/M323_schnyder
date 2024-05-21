2b8a4de480d15463300959bbdf25b9006084c6ea
Kay Schnyder <schnyderkay@gmail.com>
2024-05-21 08:34:33 +0200

    02_Anforderungen

---
+++ 01_Einführung/02_Anforderungen/02_Anforderungen/CareRace.fs
@@ -0,0 +1,20 @@
+﻿
+type LapTime = { CarId: int; LapTime: float }
+
+let calculateTotalAndAverageTimes (laptimes: LapTime list) =
+    let laptimesByCar =
+        laptimes
+        |> List.filter (fun lp -> lp.Lap > 1) // filter out warm-up lap
+        |> List.groupBy (fun lp -> lp.CarId)
+
+    let totalTimes =
+        laptimesByCar
+        |> Seq.map (fun (carId, laps) -> carId, laps |> Seq.sumBy (fun lp -> lp.LapTime))
+        |> dict
+
+    let averageTimes =
+        laptimesByCar
+        |> Seq.map (fun (carId, laps) -> carId, laps |> Seq.averageBy (fun lp -> lp.LapTime))
+        |> dict
+
+    (totalTimes, averageTimes)
---
+++ 01_Einführung/02_Anforderungen/02_Anforderungen/Destination.fs
@@ -0,0 +1,21 @@
+﻿type Destination = string
+
+type TravelRoute =
+    {
+        Destinations : Destination list
+    }
+    member this.AddDestination (destination : Destination) =
+        { this with Destinations = this.Destinations @ [destination] }
+
+    member this.ChangeDestination (index : int, newDestination : Destination) =
+        let destinations = this.Destinations
+        if index >= 0 && index < List.length destinations then
+            let newDestinations = List.take index destinations @ [newDestination] @ List.skip (index + 1) destinations
+            { this with Destinations = newDestinations }
+        else
+            failwith "Invalid index"
+
+let createTravelRoute =
+    {
+        Destinations = []
+    }
