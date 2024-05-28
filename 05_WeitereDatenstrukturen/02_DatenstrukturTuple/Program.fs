c3f4fea0df9f9d8ff648c44475c321251bc9119c
Kay Schnyder <schnyderkay@gmail.com>
2024-05-28 09:42:14 +0200

    05_WeitereDatenstrukturen

---
+++ 05_WeitereDatenstrukturen/02_DatenstrukturTuple/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+
+let wetterFunktion () =
+    let wetterbeschreibung = "sonnig" // Beispielwert
+    let currentTime = DateTime.Now
+    let temperatur = 25.0 // Beispielwert
+
+    (wetterbeschreibung, currentTime, temperatur)
+
+
+let wetterdaten : List<string * DateTime * double> = [
+    ("Stadt1", DateTime(2024, 5, 21, 9, 0, 0), 18.0)
+    ("Stadt2", DateTime(2024, 5, 21, 10, 30, 0), 22.5)
+    ("Stadt3", DateTime(2024, 5, 21, 12, 15, 0), 17.8)
+    // Weitere Daten hier hinzufügen
+]
+
+let städteÜberTemperatur (temperatur : double) =
+    wetterdaten
+    |> List.filter (fun (_, _, t) -> t > temperatur)
+    |> List.map (fun (stadt, _, _) -> stadt)
+
+// Beispielaufruf
+let städteÜber20Grad = städteÜberTemperatur 20.0
+printfn "Städte über 20 Grad: %s" (städteÜber20Grad |> String.concat ", ")
---
+++ 05_WeitereDatenstrukturen/05_WeitereDatenstrukturen/Program.fs
@@ -0,0 +1,17 @@
+﻿// 1
+let m1 = Map.ofList [("key", "value")]
+
+// 2
+let m2 = m1.Add("key2", "value2")
+
+// 3
+let m3 = m2.Add("key2", "aDifferentValue")
+
+// 4
+let m4 = m3.Remove("key")
+
+// 5
+let valueFromM3: string option = m3.TryFind("key")
+
+// 6
+let valueFromM4: string option = m3.TryFind("nonExistentKey")
